3c28d0d560a06b861040c618faaab69876391981
Chris Aporta <christopher.aporta@gmail.com>
2015-03-28 01:16:10 +0100

    wired up listeners & built out functions w/ vanilla js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,48 @@
 "use strict";
+  
+var squares = document.getElementsByClassName("square"),
+  coordinates,
+  coordinateArr = [],
+  i,
+  playButton = document.getElementById("play-button");
 
-var squares = document.getElementsByClassName("square");
-var coordinateArr = [];
-for (var i = 0; i < squares.length; i++) {
-  var coordinates = squares[i].getAttribute("id");
-  coordinateArr.push(coordinates);
+//function to addEventListener
+function addListener(obj) {
+  obj.addEventListener("click", function () {console.log("clicked")});
 }
 
-//function to retrieve a random square's coordinates
+//function to retrieve a random square's coordinates from arr
 function getRandomCoordinates() {
-    return coordinateArr[Math.floor(Math.random() * coordinateArr.length)];
+  return coordinateArr[Math.floor(Math.random() * coordinateArr.length)];
 }
 
 //function to reverse black and white perspective
 function switchSides() {
   coordinateArr.reverse();
-  for (var i = 0; i < coordinateArr.length; i++) {
+  for (i = 0; i < coordinateArr.length; i += 1) {
     squares[i].setAttribute("id", coordinateArr[i]);
   }
 }
-  
-//function to retrieve a square's coordinates
 
+//function to push results of getRandomCoordinates() to "next move" queue
+function currentCoordinate() {
+  var currentCoordinateHolder = document.getElementById("current-coordinate-holder");
+  var currentCoordinate = getRandomCoordinates();
+  currentCoordinateHolder.innerHTML = "<p>" + currentCoordinate + "</p>";
+}
+
+//function to add .progress-bar to activate timer
+function startTimer() {
+  var progBar = document.getElementById("prog-bar");
+  progBar.className = "progress-bar";
+}
+
+//add event listeners and push coordinates to array
+for (i = 0; i < squares.length; i += 1) {
+  addListener(squares[i]);
+  coordinates = squares[i].getAttribute("id");
+  coordinateArr.push(coordinates);
+}
+
+//add event listener to play button
+addListener(playButton);
