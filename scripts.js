c54ff3b725d226349978b7d79ec3cb9c1a91d503
Chris Aporta <christopher.aporta@gmail.com>
2015-05-26 16:49:10 +0200

    pretty big gap since last commit. Finalized board functionality. first push to github repo

--- scripts.js
+++ scripts.js
@@ -1,22 +1,31 @@
 "use strict";
-  
+
 var squares = document.getElementsByClassName("square"),
+  playButton = document.getElementById("play-button"),
+  progBar = document.getElementById("prog-bar"),
+  finalScoreHolder = document.getElementById("final-score-holder"),
+  currentCoordinateHolder = document.getElementById("current-coordinate-holder"),
+  chooseSide = document.getElementById("choose-side"),
   coordinates,
   coordinateArr = [],
   i,
-  playButton = document.getElementById("play-button");
+  currentMove,
+  sideVal,
+  correctAnswers = 0;
 
-//function to addEventListener
-function addListener(obj) {
-  obj.addEventListener("click", function () {console.log("clicked")});
+// push coordinates to array
+function pushToArray() {
+  for (i = 0; i < squares.length; i += 1) {
+    coordinateArr.push(squares[i].getAttribute("id"));
+  }
 }
 
-//function to retrieve a random square's coordinates from arr
+// retrieve a random square's coordinates from arr
 function getRandomCoordinates() {
   return coordinateArr[Math.floor(Math.random() * coordinateArr.length)];
 }
 
-//function to reverse black and white perspective
+// flip board
 function switchSides() {
   coordinateArr.reverse();
   for (i = 0; i < coordinateArr.length; i += 1) {
@@ -24,25 +33,103 @@ function switchSides() {
   }
 }
 
-//function to push results of getRandomCoordinates() to "next move" queue
-function currentCoordinate() {
-  var currentCoordinateHolder = document.getElementById("current-coordinate-holder");
-  var currentCoordinate = getRandomCoordinates();
-  currentCoordinateHolder.innerHTML = "<p>" + currentCoordinate + "</p>";
+function randomSide() {
+  var sideGen = Math.round(Math.random());
+  if (!sideGen) {
+    switchSides();
+  }
 }
 
-//function to add .progress-bar to activate timer
+// push random coordinates to the 'coordinate' queue
+function currentCoordinates() {
+  currentMove = getRandomCoordinates();
+  currentCoordinateHolder.innerHTML = "<p>" + currentMove + "</p>";
+  return currentMove;
+}
+
+// add .progress-bar to activate timer
 function startTimer() {
-  var progBar = document.getElementById("prog-bar");
   progBar.className = "progress-bar";
 }
 
-//add event listeners and push coordinates to array
-for (i = 0; i < squares.length; i += 1) {
-  addListener(squares[i]);
-  coordinates = squares[i].getAttribute("id");
-  coordinateArr.push(coordinates);
+function nextMove(iDynamic) {
+  return function () {
+    if (currentMove === squares[iDynamic].getAttribute("id")) {
+      currentMove = currentCoordinates();
+      correctAnswers += 1;
+      currentCoordinateHolder.style.color = "#fbfbfb";
+      return currentMove;
+    } else if (currentMove !== squares[iDynamic].getAttribute("id")) {
+      currentCoordinateHolder.style.color = "#ff0033";
+    }
+  };
 }
 
-//add event listener to play button
-addListener(playButton);
+// start game
+function startGame() {
+  currentCoordinateHolder.style.color = "#fbfbfb";
+  sideVal = chooseSide.options[chooseSide.selectedIndex].value;
+  currentMove = currentCoordinates();
+  startTimer();
+  finalScoreHolder.innerHTML = "";
+  playButton.setAttribute("disabled", true);
+  if (sideVal === "random") {
+    randomSide();
+  } else if (sideVal === "black" && coordinateArr[0] === "a8") {
+    switchSides();
+  } else if (sideVal === "white" && coordinateArr[0] === "h1") {
+    switchSides();
+  }
+}
+
+function endGame() {
+  progBar.className = "complete";
+  finalScoreHolder.innerHTML = "<p>" + correctAnswers + "<p>";
+  currentCoordinateHolder.innerHTML = "";
+  playButton.disabled = false;
+  correctAnswers = 0;
+}
+
+pushToArray();
+
+// add event listener to side picker
+chooseSide.addEventListener("change", function () {
+  if (chooseSide.options[chooseSide.selectedIndex].value !== "random") {
+    switchSides();
+  }
+}, false);
+
+// add event listeners to squares & permit move
+for (i = 0; i < squares.length; i += 1) {
+  squares[i].addEventListener("click", nextMove(i), false);
+}
+// add event listener to play button
+playButton.addEventListener("click", startGame, false);
+
+// add prefixed event listeners to progBar animation
+progBar.addEventListener("webkitAnimationEnd", endGame, false);
+progBar.addEventListener("animationend", endGame, false);
+progBar.addEventListener("oAnimationend", endGame, false);
+progBar.addEventListener("MSAnimationEnd", endGame, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
